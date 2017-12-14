8bf87778536c29c09d7837dfe58a1d925d030b8b
Midhun <midhun@qburst.com>
2017-12-14 12:39:42 +0530

    saving work

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,37 @@
-jQuery(document).ready(function() {
-	
-	"use strict";
-	// Your custom js code goes here.
-
+// Your custom js code goes here.
+$(document).ready(function() {
+  $("#ekadashi-link").on('click', function() {
+    $("#probootstrap-content .page:not('.hide')").stop().fadeOut('fast', function() {
+      $(this).addClass('hide');
+      if($('ul.probootstrap-side-menu>li').hasClass('active')){
+        $('ul.probootstrap-side-menu>li').removeClass('active')
+      }
+      // $("#sathram-link").removeClass('active');
+      // $("#sapthaham-link").removeClass('active');
+      $("#ekadashi-link").addClass('active');
+      $('#ekadashi').fadeIn('slow').removeClass('hide');
+    });
+  });
+  $("#sathram-link").on('click', function() {
+    $("#probootstrap-content .page:not('.hide')").stop().fadeOut('fast', function() {
+      $(this).addClass('hide');
+      if($('ul.probootstrap-side-menu>li').hasClass('active')){
+        $('ul.probootstrap-side-menu>li').removeClass('active')
+      }
+      // $("#ekadashi-link").removeClass('active');
+      // $("#sapthaham-link").removeClass('active');
+      $("#sathram-link").addClass('active');
+      $('#sathram').fadeIn('slow').removeClass('hide');
+    });
+  });
+  $("#sapthaham-link").on('click', function() {
+    $("#probootstrap-content .page:not('.hide')").stop().fadeOut('fast', function() {
+      $(this).addClass('hide');
+      if($('ul.probootstrap-side-menu>li').hasClass('active')){
+        $('ul.probootstrap-side-menu>li').removeClass('active')
+      }
+      $("#sapthaham-link").addClass('active');
+      $('#sapthaham').fadeIn('slow').removeClass('hide');
+    });
+  });
 });
